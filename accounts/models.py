3b655ee410e257b19d692b3884ea847d9fffd46e
Ali15471 <ali.raza@ada.ac.uk>
2026-03-17 16:25:07 +0000

    reinstalled django to set up backend correctly

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    display_name = models.CharField(max_length=80)
-    bio = models.TextField(max_length=200, blank=True)
-    profile_picture = models.URLField(blank=True)
-
-    def __str__(self):
-        return self.display_name or self.user.username
+# Create your models here.
