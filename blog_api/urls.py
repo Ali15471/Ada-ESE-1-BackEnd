14dae0d7c31d79099a6a02ebecf8364f1cbdc53d
Ali15471 <ali.raza@ada.ac.uk>
2026-03-22 14:17:21 +0000

    made migrations for posts and comments

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Welcome to the Blog API!"})
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
 ]
