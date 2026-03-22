a0536b6539b1c7ad67d7a92d7d164a945adee49a
Ali15471 <ali.raza@ada.ac.uk>
2026-03-22 19:27:38 +0000

    Created serializers file for comments

---
+++ comments/serializers.py
@@ -0,0 +1,15 @@
+from rest_framwork import serializers
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author_username', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'post', 'author_username', 'created_at', 'updated_at']
+
+        def validate_content(self, value):
+            if not value.strip():
+                raise serializers.ValidationError("Content cannot be empty.")
+            return value
